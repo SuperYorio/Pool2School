bfc5714533bceba93960e62babb08a5954dc584d
SuperYorio <47366731+SuperYorio@users.noreply.github.com>
2019-09-10 23:34:44 -0700

    Update editInfo.js

--- editInfo.js
+++ editInfo.js
@@ -20,10 +20,11 @@ var password;
 var childData;
 var userKey;
 var userName;
+var currentEmail;
 
 // This method returns the information of a user
 ref.on("value", function (snapshot) {
-    var currentEmail = firebase.auth().currentUser.email;
+    currentEmail = firebase.auth().currentUser.email;
     console.log("Email:  " + currentEmail);
     snapshot.forEach(function (childSnapshot) {
         childData = childSnapshot.val();
@@ -63,10 +64,11 @@ function submitForm(e) {
     var city = getInputVal('editInputCity');
     var state = getInputVal('editInputState');
     var zip = getInputVal('editInputZip');
+    var days = get_days_to_pool();
 
-    data = {City: city, Email: email, FirstAddress: firstAddress, FirstName: firstName, Gender: gender,
+    data = {City: city, Email: currentEmail, FirstAddress: firstAddress, FirstName: firstName, Gender: gender,
         LastName: lastName, Password: password, PhoneNumber: phoneNum, SecondAddress: secondAddress,
-        State: state, Zip: zip}
+        State: state, Zip: zip, DaysToPool: days};
 
     const fb = firebase.database().ref();
     console.log(userKey);
@@ -80,4 +82,59 @@ function submitForm(e) {
 
 function getInputVal(id) {
     return document.getElementById(id).value;
+}
+
+function get_days_to_pool() {
+    var darray = new Array(7);
+
+    if(document.getElementById("monday").checked) {
+        darray[0] = document.getElementById("monday").value;
+    } else {
+        darray[0] = "";
+    }
+
+    if(document.getElementById("tuesday").checked) {
+        darray[1] = document.getElementById("tuesday").value;
+    } else {
+        darray[1] = "";
+    }
+
+    if(document.getElementById("wednesday").checked) {
+        darray[2] = document.getElementById("wednesday").value;
+    } else {
+        darray[2] = "";
+    }
+
+    if(document.getElementById("thursday").checked) {
+        darray[3] = document.getElementById("thursday").value;
+    } else {
+        darray[3] = "";
+    }
+
+    if(document.getElementById("friday").checked) {
+        darray[4] = document.getElementById("friday").value;
+    } else {
+        darray[4] = "";
+    }
+
+    if(document.getElementById("saturday").checked) {
+        darray[5] = document.getElementById("saturday").value;
+    } else {
+        darray[5] = "";
+    }
+
+    if(document.getElementById("sunday").checked) {
+        darray[6] = document.getElementById("sunday").value;
+    } else {
+        darray[6] = "";
+    }
+
+    var days = "";
+    for(var i = 0; i < 7; i++) {
+        if(darray[i] == "") {
+            continue;
+        }
+        days += " " + darray[i];
+    }
+    return days.substring(1);
 }
