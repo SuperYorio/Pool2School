13059283e3758e2234844c930f70f4d6b190d221
Zhiwei-Z <zhiweizhang2012@gmail.com>
2019-09-11 15:45:58 -0700

    fix retrieve

--- RetrieveUser.js
+++ RetrieveUser.js
@@ -15,9 +15,20 @@ if (!firebase.apps.length) {
 
 // BUG: The users_array returned is an empty array/undefined because the data
 // wasn't loaded yet
-var users_array = retrieve_all_info();
-console.log("Calling All Users: ");
-console.log(users_array);
+var users_array = [];
+let p = retrieve_all_info();
+p.then(function(result){
+    console.log("fetched");
+    users_array = result;
+    console.log("Calling All Users: ");
+    console.log(users_array);
+    /**
+     * TODO: THE rest of the code has to go here.
+     */
+}).catch(function () {
+    console.log("no user fetched, check connection or database");
+});
+
 
 // The method that returns all users in an array of objects
 // array[0] is the original user
@@ -25,21 +36,47 @@ console.log(users_array);
 // And user-M logged in, so the ideal output array would be
 // [M, A, B, C, D, ... (skips M), X, Y, Z]
 function retrieve_all_info() {
-    var ref = firebase.database().ref("Student");
-    // The array we want full of users, or Students, so to speak.
-    var all_users = [];
-    // This method returns the information of a user
-    ref.on("value", function (snapshot) {
-        var currentEmail = firebase.auth().currentUser.email;
-        console.log("Email:  " + currentEmail);
-        // Snapshot used to retrieve information
-        snapshot.forEach(function (childSnapshot) {
-            childData = childSnapshot.val();
-            var email = childData.Email;
-            var user_address;
-            if(email.toString() == currentEmail.toString()) {
-                // The orig_dict is basically just the Student object
-                var orig_dict = {
+    return new Promise(function(resolve, reject){
+        var ref = firebase.database().ref("Student");
+        // The array we want full of users, or Students, so to speak.
+        var all_users = [];
+        // This method returns the information of a user
+        ref.on("value", function (snapshot) {
+            var currentEmail = firebase.auth().currentUser.email;
+            console.log("Email:  " + currentEmail);
+            // Snapshot used to retrieve information
+            snapshot.forEach(function (childSnapshot) {
+                childData = childSnapshot.val();
+                var email = childData.Email;
+                var user_address;
+                if(email.toString() == currentEmail.toString()) {
+                    // The orig_dict is basically just the Student object
+                    var orig_dict = {
+                        userKey: childSnapshot.key,
+                        email: email,
+                        password: childData.Password,
+                        user_name: childData.FirstName + " " + childData.LastName,
+                        address: user_address,
+                        gender: childData.Gender,
+                        phoneNum: childData.PhoneNumber,
+                        distance: 0
+                    };
+                    // This pushes the user we are searching with to the head of the array (hence orig)
+                    all_users.unshift(orig_dict);
+                    included_self = true;
+                    return;
+                }
+                // Basic address formatting
+                if(childData.SecondAddress.toString() == "") {
+                    user_address = childData.FirstAddress + ", " +
+                        childData.City + ", " + childData.State + ", " + childData.Zip;
+                }
+                else {
+                    user_address = childData.FirstAddress + ", " + childData.SecondAddress + ", " +
+                        childData.City + ", " + childData.State + ", " + childData.Zip;
+                }
+                // Other users
+                var user_dict = {
                     userKey: childSnapshot.key,
                     email: email,
                     password: childData.Password,
@@ -49,35 +86,19 @@ function retrieve_all_info() {
                     phoneNum: childData.PhoneNumber,
                     distance: 0
                 };
-                // This pushes the user we are searching with to the head of the array (hence orig)
-                all_users.unshift(orig_dict);
-                included_self = true;
-                return;
+                // This adds all other users into the array (end)
+                all_users.push(user_dict);
+
+            });
+            if(all_users.length !== 0){
+                console.log("what");
+                resolve(all_users);
+            }else{
+                reject();
             }
-            // Basic address formatting
-            if(childData.SecondAddress.toString() == "") {
-                user_address = childData.FirstAddress + ", " +
-                    childData.City + ", " + childData.State + ", " + childData.Zip;
-            }
-            else {
-                user_address = childData.FirstAddress + ", " + childData.SecondAddress + ", " +
-                    childData.City + ", " + childData.State + ", " + childData.Zip;
-            }
-            // Other users
-            var user_dict = {
-                userKey: childSnapshot.key,
-                email: email,
-                password: childData.Password,
-                user_name: childData.FirstName + " " + childData.LastName,
-                address: user_address,
-                gender: childData.Gender,
-                phoneNum: childData.PhoneNumber,
-                distance: 0
-            };
-            // This adds all other users into the array (end)
-            all_users.push(user_dict);
+
         });
+        // Return the user array
+
     });
-    // Return the user array
-    return all_users;
 }
